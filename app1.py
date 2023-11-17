57f816888d3931e429643a009a00c74c26c17f3d
geetkarthi <49567491+geetkarthi@users.noreply.github.com>
2023-11-17 16:34:04 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -23,7 +23,7 @@ df_original = df_original[df_original['year'] == selectbox_b]
 df_original['difference'] = df_original['xG'] - df_original['xGA']
 df_original['points_based_on_xG'] = np.where(df_original['difference'] > 0, 3, 0)
 df_new = pd.melt(df_original, id_vars = ['date'], value_vars = ['pts', 'points_based_on_xG'])
-plt.figure(figsize = (100, 50))
+plt.figure(figsize = (80, 40))
 x = sns.barplot(df_new, x = 'date', y = 'value', hue = 'variable')
 #The above figure plots the points the team got versus the points they should have got based on their xG for the matchday
 st.pyplot(x.get_figure())
